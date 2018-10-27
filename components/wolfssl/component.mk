d81221a267210ddc0a44cd3a3652343e771008eb
Hideki Miyazaki <hide@wolfssl.com>
2018-10-27 22:20:28 +0900

    Added component.mk file for wolfssl build

---
+++ components/wolfssl/component.mk
@@ -0,0 +1,12 @@
+#
+# Component Makefile
+#
+
+COMPONENT_ADD_INCLUDEDIRS := .
+
+COMPONENT_SRCDIRS := src wolfcrypt/src
+
+COMPONENT_OBJEXCLUDE := wolfcrypt/src/aes_asm.o
+COMPONENT_OBJEXCLUDE += wolfcrypt/src/evp.o
+COMPONENT_OBJEXCLUDE += wolfcrypt/src/misc.o
+COMPONENT_OBJEXCLUDE += src/bio.o
